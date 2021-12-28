a7ac34b41eb5a1a116fc2cc2bea7b944b8aff765
Ahmad Katrib <ahmadkatrib123@gmail.com>
2021-12-28 15:40:13 +0200

    step 4

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ app.get("/time",(req,res)=>{
     let date = new Date()
     res.send({status:200, message:`${date.getHours()}:${date.getMinutes()}`})
 })
+//step 4 
+app.get(["/hello","/hello/:id"],(req,res)=>{
+    res.send({status:200, message:`Hello, ${req.params.id || "Unkwon"}`})
+})
+
+app.get("/search",(req,res)=>{
+    if(typeof req.query.s =="undefined" || req.query.s === "") res.send({status:500, error:true, message:"you have to provide a search"})
+    else res.send( {status:200, message:"ok", data:req.query.s})
+})
+
