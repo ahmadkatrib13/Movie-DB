90e3a1bbec9626f3eddcbf56fb3ba96c02e99664
Ahmad Katrib <ahmadkatrib123@gmail.com>
2021-12-28 15:20:07 +0200

    step 3

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 app.listen(3000)
-app.get(/\//, (req, res) => {
-  res.send('Ok')
+//step 2
+// app.get(/\//, (req, res) => {
+//   res.send('Ok')
+// })
+
+//step 3
+app.get("/test",(req,res)=>{
+    res.send( {status:200, message:"ok"})
+})
+app.get("/time",(req,res)=>{
+    let date = new Date()
+    res.send({status:200, message:`${date.getHours()}:${date.getMinutes()}`})
 })
