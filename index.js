ce79f1ca7791735445fba924f2f1e9749f0017a7
ahmadkatrib13 <ahmadkatrib123@gmail.com>
2021-12-28 21:37:40 +0200

    step 6

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ app.listen(3000)
 // })
 
 //step 3
-app.get("/test",(req,res)=>{
-    res.send( {status:200, message:"ok"})
+app.get("/test", (req, res) => {
+    res.send({ status: 200, message: "ok" })
 })
-app.get("/time",(req,res)=>{
+app.get("/time", (req, res) => {
     let date = new Date()
-    res.send({status:200, message:`${date.getHours()}:${date.getMinutes()}`})
+    res.send({ status: 200, message: `${date.getHours()}:${date.getMinutes()}` })
 })
 //step 4 
-app.get(["/hello","/hello/:id"],(req,res)=>{
-    res.send({status:200, message:`Hello, ${req.params.id || "Unkwon"}`})
+app.get(["/hello", "/hello/:id"], (req, res) => {
+    res.send({ status: 200, message: `Hello, ${req.params.id || "Unkwon"}` })
 })
 
-app.get("/search",(req,res)=>{
-    if(typeof req.query.s =="undefined" || req.query.s === "") res.send({status:500, error:true, message:"you have to provide a search"})
-    else res.send( {status:200, message:"ok", data:req.query.s})
+app.get("/search", (req, res) => {
+    if (typeof req.query.s == "undefined" || req.query.s === "") res.send({ status: 500, error: true, message: "you have to provide a search" })
+    else res.send({ status: 200, message: "ok", data: req.query.s })
 })
 
 //step 5
@@ -32,15 +32,33 @@ const movies = [
     { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
 ]
 
-app.get("/movies/create",(req,res)=>{
+app.get("/movies/create", (req, res) => {
     res.send("create a movie")
 })
-app.get("/movies/read",(req,res)=>{
-    res.send({status:200, data:movies })
+app.get("/movies/read", (req, res) => {
+    console.log(movies);
+    res.send({ status: 200, data: movies })
 })
-app.get("/movies/update",(req,res)=>{
+app.get("/movies/update", (req, res) => {
     res.send("update a movie")
 })
-app.get("/movies/delete",(req,res)=>{
+app.get("/movies/delete", (req, res) => {
     res.send("delete a movie")
 })
+
+//step 6
+app.get("/movies/read/by-date", (req, res) => {
+    res.send({ status: 200, data: [...movies].sort((a, b) => b.year - a.year) })
+});
+
+app.get("/movies/read/by-rating", (req, res) => {
+    res.send({
+        status: 200, data: [...movies].sort((a, b)=>b.rating-a.rating)
+    })
+});
+
+app.get("/movies/read/by-title", (req, res) => {
+    res.send({
+        status: 200, data: [...movies].sort((a, b) => (a.title).localeCompare(b.title))
+    })
+});
