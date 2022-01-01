269ee72c5b9004ea17b17bcc2e3995cefaade037
ahmadkatrib13 <ahmadkatrib123@gmail.com>
2022-01-01 17:25:11 +0200

    step 11

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.listen(3000)
 //   res.send('Ok')
 // })
 //--for just the root route
-// app.get("/", (req, res) => {
-//   res.send('Ok')
-// })
+app.get("/", (req, res) => {
+  res.send('Ok')
+})
 
 //step 3
 app.get("/test", (req, res) => {
@@ -38,16 +38,16 @@ const movies = [
     { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
 ]
 
-app.get("/movies/create", (req, res) => {
+app.post("/movies/create", (req, res) => {
     res.send("create a movie")
 })
 app.get("/movies/read", (req, res) => {
     res.send({ status: 200, data: movies })
 })
-app.get("/movies/update", (req, res) => {
+app.put("/movies/update", (req, res) => {
     res.send("update a movie")
 })
-app.get("/movies/delete", (req, res) => {
+app.delete("/movies/delete", (req, res) => {
     res.send("delete a movie")
 })
 
@@ -67,14 +67,14 @@ app.get("/movies/read/by-title", (req, res) => {
         status: 200, data: [...movies].sort((a, b) => (a.title).localeCompare(b.title))
     })
 });
-
+//step 7
 app.get(["/movies/read/id/","/movies/read/id/:id"], (req, res) => {
     let id = Number(req.params.id)
     if(id>=0 && id<movies.length) res.send({status:200, data:movies[id]});
     else res.status(404).send({status:404, error:true, message:`the movie '${req.params.id || 'Unknown'}' does not exist`});
 });
-
-app.get("/movies/add",(req, res) => {
+//step 8
+app.post("/movies/add",(req, res) => {
     //?title=<TITLE>&year=<YEAR>&rating=<RATING>
     if(req.query.title && req.query.year && (/^[1-9]\d{3}$/).test(req.query.year)){
     // title not missing  //year not missing //4 digits and a number
@@ -83,16 +83,16 @@ app.get("/movies/add",(req, res) => {
     res.send({ status: 200, data: movies })
     }else res.status(403).send({status:403, error:true, message:'you cannot create a movie without providing a title and a year'});
 });
-
-app.get("/movies/delete/:id", (req, res) => {
+//step 9
+app.delete("/movies/delete/:id", (req, res) => {
     let id = Number(req.params.id)
     if(id>=0 && id<movies.length){
         movies.splice(id,1);
         res.send({status:200, data:movies});
     }else res.status(404).send({status:404, error:true, message:`the movie '${req.params.id}' does not exist`});
 });
-
-app.get("/movies/update/:id", (req, res) => {
+//step 10
+app.put("/movies/update/:id", (req, res) => {
     let id = Number(req.params.id)
     //check if the id exist 
     if(id>=0 && id<movies.length){
@@ -107,3 +107,5 @@ app.get("/movies/update/:id", (req, res) => {
 
     }else res.status(404).send({status:404, error:true, message:`the movie '${req.params.id}' does not exist`});
 });
+
+//step 11 done
