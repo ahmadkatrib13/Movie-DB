75118166eb194b3743cd19b675f75bea9e73bee1
ahmadkatrib13 <73841911+ahmadkatrib13@users.noreply.github.com>
2022-01-10 10:47:24 +0200

    fix port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
-app.listen(4000)
+app.listen(3000)
 app.use(express.json());
 
 const uri = "mongodb+srv://admin:admin@cluster0.7go78.mongodb.net/movie-db?retryWrites=true&w=majority"
